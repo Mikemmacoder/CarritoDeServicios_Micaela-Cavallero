f92e18b80c0acaf019783d1aca8859d4858a5536
mikemma <micaelafcavallero@gmail.com>
2023-05-19 15:17:46 -0300

    fetch funcionando con json

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const modalidadServicio = ['Presencial', 'Virtual'];
+/* const modalidadServicio = ['Presencial', 'Virtual']; */
 let valorTotal = 0;
 let precios = 0;
 let botonVaciar = document.getElementById('vaciar-carrito-btn');
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
     mostrarItemsCarrito();
 });
 
-class Servicios {
+/* class Servicios {
     constructor(id, nombre, modalidad, duracion, minimo, precio) {
         this.id = id;
         this.nombre = nombre;
@@ -32,31 +32,30 @@ const servicio1 = new Servicios(1, 'Coaching personal', modalidadServicio, 1, 3,
 const servicio2 = new Servicios(2, 'Coaching de parejas', modalidadServicio, 1, 3, 6000);
 const servicio3 = new Servicios(3, 'Coaching grupal', modalidadServicio, 4, 3, 30000);
 
-const servicios = [servicio1, servicio2, servicio3];
+const servicios = [servicio1, servicio2, servicio3]; */
 /* const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
 
 // o almacenar array completo
 guardarLocal("listaServicios", JSON.stringify(servicios)); */
+const servicios = [];
 
-fetch("servicios.json") // Ruta relativa al archivo "servicios.json"
+fetch("js/servicios.json") // Ruta relativa al archivo "servicios.json"
     .then((response) => response.json())
     .then((data) => {
         // Aquí puedes utilizar los datos obtenidos del archivo "servicios.json"
         console.log(data);
+        for (let servicio of data) {
+            servicios.push(servicio)
+        }
+        console.log("servicios" + servicios)
 
         // Resto de tu código para procesar los datos
         // ...
-    })
-    .catch((error) => {
-        // Manejo de errores en caso de que no se pueda acceder al archivo "servicios.json"
-        console.error("Error al obtener los datos del archivo servicios.json:", error);
-    });
-
-let articuloCartas = document.getElementById('cartas');
-for (let servicio of servicios) {
-    let carta = document.createElement('div');
-    carta.className = 'card col-md-3 col-sm-5 cartaServicio';
-    carta.innerHTML = `
+        let articuloCartas = document.getElementById('cartas');
+        for (let servicio of data) {
+            let carta = document.createElement('div');
+            carta.className = 'card col-md-3 col-sm-5 cartaServicio';
+            carta.innerHTML = `
         <div class="card-body">
             <h5 class="card-title">${servicio.nombre}</h5>
             <p class="card-text">${'Modalidades: ' + servicio.modalidad}</p>
@@ -64,41 +63,55 @@ for (let servicio of servicios) {
             <p class="card-text">Precio por encuentro $ ${servicio.precio}</p>
             <button id="btn${servicio.id}" class="btn btn-primary comprar">Comprar</button>
         </div>`;
-    articuloCartas.append(carta);
-}
+            articuloCartas.append(carta);
+        }
 
-let boton1 = document.getElementById('btn1');
-let boton2 = document.getElementById('btn2');
-let boton3 = document.getElementById('btn3');
+        let boton1 = document.getElementById('btn1');
+        let boton2 = document.getElementById('btn2');
+        let boton3 = document.getElementById('btn3');
 
-const resultado = document.getElementById('total');
-    resultado.innerHTML = `
-    <h3>Total $0</h3>`;
+        const resultado = document.getElementById('total');
+        resultado.innerHTML = `<h3>Total $0</h3>`;
 
-boton1.addEventListener("click", () => {
-    carrito.push(servicio1);
-    alert('Se agregó el servicio al carrito!');
-    console.table(carrito);
-    mostrarItemsCarrito();
-    calcularTotal()
-    guardarCarrito()
-});
-boton2.addEventListener("click", () => {
-    carrito.push(servicio2);
-    alert('Se agregó el servicio al carrito!');
-    console.table(carrito);
-    mostrarItemsCarrito();
-    calcularTotal()
-    guardarCarrito()
-});
-boton3.addEventListener("click", () => {
-    carrito.push(servicio3);
-    alert('Se agregó el servicio al carrito!');
-    console.table(carrito);
-    mostrarItemsCarrito();
-    calcularTotal()
-    guardarCarrito()
-});
+        boton1.addEventListener("click", () => {
+            carrito.push(servicios[0]);
+            alert('Se agregó el servicio al carrito!');
+            console.table(carrito);
+            mostrarItemsCarrito();
+            calcularTotal()
+            guardarCarrito()
+        });
+        boton2.addEventListener("click", () => {
+            carrito.push(servicios[1]);
+            alert('Se agregó el servicio al carrito!');
+            console.table(carrito);
+            mostrarItemsCarrito();
+            calcularTotal()
+            guardarCarrito()
+        });
+        boton3.addEventListener("click", () => {
+            carrito.push(servicios[2]);
+            alert('Se agregó el servicio al carrito!');
+            console.table(carrito);
+            mostrarItemsCarrito();
+            calcularTotal()
+            guardarCarrito()
+        });
+
+
+
+
+
+
+
+
+    })
+    .catch((error) => {
+        // Manejo de errores en caso de que no se pueda acceder al archivo "servicios.json"
+        console.error("Error al obtener los datos del archivo servicios.json:", error);
+    });
+
+
 
 function mostrarItemsCarrito() {
     const tabla = document.getElementById('items');
@@ -122,7 +135,7 @@ function calcularTotal() {
     let serTotal = carrito.map((servicio) => servicio.precio)
     console.log(serTotal);
     valorTotal = serTotal.reduce((acumulador, elemento) => acumulador + elemento, 0)
-    
+
     const resultado = document.getElementById('total');
     resultado.innerHTML = ``;
     resultado.innerHTML += `
